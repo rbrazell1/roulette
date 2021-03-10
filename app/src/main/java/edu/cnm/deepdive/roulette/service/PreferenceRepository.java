f521bebbb5592fde9eba3e4777b706768287bf31
rbrazell1 <brazellrussell@gmail.com>
2021-03-10 16:58:08 -0700

    Added classes for default values, and PreferenceRepository that call to the correct resources

---
+++ app/src/main/java/edu/cnm/deepdive/roulette/service/PreferenceRepository.java
@@ -0,0 +1,38 @@
+package edu.cnm.deepdive.roulette.service;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import androidx.preference.PreferenceManager;
+import edu.cnm.deepdive.roulette.R;
+
+public class PreferenceRepository {
+
+  private final Context context;
+  private final SharedPreferences preferences;
+  private final Resources resources;
+
+
+  public PreferenceRepository(Context context) {
+    this.context = context;
+    preferences = PreferenceManager.getDefaultSharedPreferences(context);
+    resources = context.getResources();
+  }
+
+  public int getMinimumWager() {
+    return preferences.getInt(resources.getString(R.string.minimum_wager_key),
+    resources.getInteger(R.integer.minimum_wager_default));
+  }
+
+  public int getStartingPot() {
+return preferences.getInt(resources.getString(R.string.starting_pot_key),
+    resources.getInteger(R.integer.starting_pot_default));
+  }
+
+  public boolean isLetItRide() {
+return preferences.getBoolean(resources.getString(R.string.let_it_ride_key),
+    resources.getBoolean(R.bool.let_it_ride_default));
+  }
+
+}
