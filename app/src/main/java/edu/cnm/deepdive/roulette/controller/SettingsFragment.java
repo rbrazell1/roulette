2cc0831a8589dfb2223fd135234fde561a9455de
rbrazell1 <brazellrussell@gmail.com>
2021-03-11 09:18:37 -0700

    Added setting for starting por increment and added icons

--- app/src/main/java/edu/cnm/deepdive/roulette/controller/SettingsFragment.java
+++ app/src/main/java/edu/cnm/deepdive/roulette/controller/SettingsFragment.java
@@ -2,6 +2,7 @@ package edu.cnm.deepdive.roulette.controller;
 
 import android.os.Bundle;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SeekBarPreference;
 import edu.cnm.deepdive.roulette.R;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
@@ -9,5 +10,21 @@ public class SettingsFragment extends PreferenceFragmentCompat {
   @Override
   public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
     setPreferencesFromResource(R.xml.preference,rootKey);
+    forceStartingPotIncrement();
+  }
+
+  private void forceStartingPotIncrement() {
+    SeekBarPreference startingPot = findPreference(getString(R.string.starting_pot_key));
+    int increment = getResources().getInteger(R.integer.starting_pot_increment);
+    startingPot.setOnPreferenceChangeListener((preference, newValue) -> {
+      int value = (Integer) newValue;
+      boolean save = true;
+      if (value % increment != 0) {
+        value = Math.round((float) value / increment) * increment;
+        startingPot.setValue(value);
+        save = false;
+      }
+      return save;
+    });
   }
 }
